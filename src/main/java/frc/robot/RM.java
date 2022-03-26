203c4dfc4cd1379db8983974b5b58c680ec6b152
unknown <dkamen74@gmail.com>
2022-03-26 11:02:38 -0500

    mapping update

--- src/main/java/frc/robot/RM.java
+++ src/main/java/frc/robot/RM.java
@@ -3,21 +3,21 @@ package frc.robot;
 public class RM {
     //Drive wheels
     //TalonSRX
-    public static final int frontLeft = 1;
-    public static final int frontRight = 2;
-    public static final int backLeft = 3;
-    public static final int backRight = 4;
+    public static final int frontLeft = 11;
+    public static final int frontRight = 12;
+    public static final int backLeft = 13;
+    public static final int backRight = 14;
 
     //Spark
     public static final int intake = 5;
     public static final int shooter = 6;
 
     //Pneumatics
-    public static final int intakeRup = 3;
-    public static final int intakeRdown = 4;
-    public static final int intakeLup = 5;
-    public static final int intakeLdown = 6;
-    public static final int shootup = 7;
-    public static final int shootdown = 8;
-    public static final int compressor = 9;
+    public static final int intakeRup = 1;
+    public static final int intakeRdown = 2;
+    public static final int intakeLup = 3;
+    public static final int intakeLdown = 4;
+    public static final int shootup = 5;
+    public static final int shootdown = 6;
+    public static final int compressor = 7;
 }
